f67020068ff9619938aa43d3869849b63e80551a
VietNH <vietpiano@gmail.com>
2016-05-17 14:40:29 +0700

    Update Comment

--- socket_server/server/init.js
+++ socket_server/server/init.js
@@ -1,9 +1,14 @@
-var io = require('socket.io')(3000); // Create 
-var redis = require('socket.io-redis');
-io.adapter(redis({ host: 'localhost', port: 6379 }));
-console.log("Server listening on 3000");
+/***
+ * Server Side using NodeJS and Socket.io
+ * Author: Nguyen Hoang Viet
+ * Datecreate 17-05-2016
+ */
+var io = require('socket.io')(3000); // import socket.io with listen port at 3000
+var redis = require('socket.io-redis'); // import socket.io-redis to save socket into redis server for Loadbalancing Request
+io.adapter(redis({ host: 'localhost', port: 6379 })); // Config socket.io connect to redis port
+console.log("Server listening on 3000"); // print welcome message
 
-io.sockets.on('connection', function (socket) {
+io.sockets.on('connection', function (socket) { // Listen action from client
     var deviceid = "123";
     socket.join(deviceid);
 
